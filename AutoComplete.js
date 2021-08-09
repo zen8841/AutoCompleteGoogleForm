baa075d07397e3e86b4850c25fb9615b3560daad
zen8841 <59140857+zen8841@users.noreply.github.com>
2021-08-09 14:58:48 +0800

    Support Biology Class Form

--- AutoComplete.js
+++ AutoComplete.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AutoComplete
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.1
 // @description  Fill the form automatically
 // @author       Zen Wen
 // @match        https://docs.google.com/forms/*
@@ -11,23 +11,33 @@
 
 (function() {
     'use strict';
+    const userData = {
+        Class : yourClass,
+        Seat : yourSeatNum,
+        Name : "yourName",
+        ID : yourID,
+        Food : "飯"
+    };
+    var oldUrlPath = window.location.pathname;
+    var newURL = oldUrlPath + "&?123";
     if(FB_PUBLIC_LOAD_DATA_[1][1][0][1] === "班級"){
-        const userData = {
-            Class : 217,
-            Seat : 1,
-            Name : /*"your name"*/,
-            ID : /*Your ID last 5 Number*/
-        };
         const entryPoint = {
             Class : FB_PUBLIC_LOAD_DATA_[1][1][0][4][0][0],
             Seat :  FB_PUBLIC_LOAD_DATA_[1][1][1][4][0][0],
             Name :  FB_PUBLIC_LOAD_DATA_[1][1][2][4][0][0],
             ID :    FB_PUBLIC_LOAD_DATA_[1][1][3][4][0][0]
         };
-        var oldUrlPath = window.location.pathname;
-        var newURL = oldUrlPath + `?entry.${entryPoint.Class}=${userData.Class}&entry.${entryPoint.Seat}=${userData.Seat}&entry.${entryPoint.Name}=${userData.Name}&entry.${entryPoint.ID}=${userData.ID}` + "&?123";
-        window.location.replace (newURL);
+        newURL = oldUrlPath + `?entry.${entryPoint.Class}=${userData.Class}&entry.${entryPoint.Seat}=${userData.Seat}&entry.${entryPoint.Name}=${userData.Name}&entry.${entryPoint.ID}=${userData.ID}` + "&?123";
     }
+    else if(FB_PUBLIC_LOAD_DATA_[1][1][0][1] == "座號"){
+        const entryPoint = {
+            Seat : FB_PUBLIC_LOAD_DATA_[1][1][0][4][0][0],
+            Name : FB_PUBLIC_LOAD_DATA_[1][1][1][4][0][0],
+            Food : FB_PUBLIC_LOAD_DATA_[1][1][2][4][0][0],
+        };
+        newURL = oldUrlPath + `?entry.${entryPoint.Seat}=${userData.Seat}&entry.${entryPoint.Name}=${userData.Name}&entry.${entryPoint.Food}=${userData.Food}` + "&?123";
+    }
+    window.location.replace (newURL);
 })();
 // x = number of question
 // FB_PUBLIC_LOAD_DATA_[1][1][x][1] = title of question x
